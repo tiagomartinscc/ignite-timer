1534f917f1214e3d0d9946f577d09284e5fa284d
Tiago Martins <tiagomartinscc@hotmail.com>
2024-08-20 07:05:12 -0300

    styling the layout component

--- src/styles/globals.ts
+++ src/styles/globals.ts
@@ -8,18 +8,18 @@ export const GlobalStyle = createGlobalStyle`
   }
 
   :focus {
-    outline: 0;
+    outline: none;
     box-shadow: 0 0 0 2px ${(props) => props.theme['green-500']};
   }
 
   body {
-    background-color: ${(props) => props.theme['gray-900']};
+    background: ${(props) => props.theme['gray-900']};
     color: ${(props) => props.theme['gray-300']};
   }
 
-  body, button, input, textarea {
-    font: 'Roboto', sans-serif;
+  body, input, textarea, button {
+    font-family: 'Roboto', sans-serif;
     font-weight: 400;
     font-size: 1rem;
-  } 
+  }
 `
