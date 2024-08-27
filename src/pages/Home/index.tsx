983ca829ceeb4c822e3c966bd0cc4b7594f80973
Tiago Martins <tiagomartinscc@hotmail.com>
2024-08-27 07:20:20 -0300

    start a countdown

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -2,6 +2,8 @@ import { Play } from 'phosphor-react'
 import { useForm } from 'react-hook-form'
 import { zodResolver } from '@hookform/resolvers/zod'
 import * as zod from 'zod'
+import { useEffect, useState } from 'react'
+import { differenceInSeconds } from 'date-fns'
 
 import {
   CountdownContainer,
@@ -12,7 +14,6 @@ import {
   StartCountdownButton,
   TaskInput,
 } from './styles'
-import { useState } from 'react'
 
 const newCycleFormValidationSchema = zod.object({
   task: zod.string().min(1, 'Informe a tarefa'),
@@ -28,6 +29,7 @@ interface Cycle {
   id: string
   task: string
   minutesAmount: number
+  startDate: Date
 }
 
 export function Home() {
@@ -43,25 +45,36 @@ export function Home() {
     },
   })
 
-  function handleCreateNewCycle({ task, minutesAmount }: NewCycleFormData) {
+  const activeCycle = cycles.find((cycle) => cycle.id === activeCycleId)
+
+  useEffect(() => {
+    if (activeCycle) {
+      setInterval(() => {
+        setAmountSecondsPassed(
+          differenceInSeconds(new Date(), activeCycle.startDate),
+        )
+      }, 1000)
+    }
+  }, [activeCycle])
+
+  function handleCreateNewCycle(data: NewCycleFormData) {
     const id = String(new Date().getTime())
 
     const newCycle: Cycle = {
       id,
-      task,
-      minutesAmount,
+      task: data.task,
+      minutesAmount: data.minutesAmount,
+      startDate: new Date(),
     }
-    // lembrar que, toda vez que eu precisar pegar o valor atualizado do estado,
-    // deve ser feito atraves de função anonima.
+
     setCycles((state) => [...state, newCycle])
     setActiveCycleId(id)
+
     reset()
   }
 
-  const cycleActive = cycles.find((cycle) => cycle.id === activeCycleId)
-
-  const totalSeconds = cycleActive ? cycleActive.minutesAmount * 60 : 0
-  const currentSeconds = cycleActive ? totalSeconds - amountSecondsPassed : 0
+  const totalSeconds = activeCycle ? activeCycle.minutesAmount * 60 : 0
+  const currentSeconds = activeCycle ? totalSeconds - amountSecondsPassed : 0
 
   const minutesAmount = Math.floor(currentSeconds / 60)
   const secondsAmount = currentSeconds % 60
@@ -70,7 +83,7 @@ export function Home() {
   const seconds = String(secondsAmount).padStart(2, '0')
 
   const task = watch('task')
-  const isSubmitDisabled = !task
+  const isSubmitDisable = !task
 
   return (
     <HomeContainer>
@@ -113,7 +126,7 @@ export function Home() {
           <span>{seconds[1]}</span>
         </CountdownContainer>
 
-        <StartCountdownButton disabled={isSubmitDisabled} type="submit">
+        <StartCountdownButton disabled={isSubmitDisable} type="submit">
           <Play size={24} />
           Começar
         </StartCountdownButton>
